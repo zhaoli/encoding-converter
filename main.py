36f2ba54eed279b20995bb836fd20f32827e4c0c
zhaoli <zhaoli.cd@gmail.com>
2013-08-16 15:24:16 +0800

    minor improvement.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import codecs
 
 def encode_converter(file_name):
     """检测文件编码，如果是utf-8则通过，如果是其他编码转换成utf-8"""
-    # 读取文件，是否可以只读前面100个字节？
-    source_file = open(file_name, 'rb')
+    # 读取文件
+    source_file = open(file_name, 'rb') #需要以bytes模式打开
     print(file_name)
     content = source_file.read()
     source_file.close()
@@ -17,8 +17,10 @@ def encode_converter(file_name):
     if result['encoding'] == 'utf-8':
         pass
     else:
+        #以检测到的编码重新读取内容，得到正确的文本
         temp_content = content.decode(result['encoding'],'ignore')
         print(temp_content)
+        #文件重新打开，编码改为utf－8，并清空
         dist_file = open(file_name, 'w', encoding='utf-8')
         dist_file.truncate()
         dist_file.write(temp_content) # 重新编码成utf-8，写入
@@ -36,10 +38,12 @@ else:
 
 
 # 指定文件类型：srt/ass/txt/html
-file_type = input('请指定要转换的文件类型：srt、ass、txt、html或其他\n')
+file_type = '.' + input('请指定要转换的文件类型：srt、ass、txt、html或其他\n')
+# 检查文件夹，处理符合条件的文件
 for files in file_list:
     if file_type in files:
         encode_converter(files)
+print('处理完毕！')
 
 
 
